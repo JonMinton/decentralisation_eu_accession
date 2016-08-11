32cdb54958b5f41cbddf5a5501d122eea6fc280a
Jonathan Minton <nate.minton@gmail.com>
2016-08-11 16:18:01 +0100

    get stuck realising records from 2011 are missing

--- script.R
+++ script.R
@@ -37,7 +37,7 @@ dta <- read_csv("data/derived/simplified_both.csv")
 # I already have LSOAs to MSOA lookup for 2001
 
 lookup <- read_csv("data/2001_geographical_lookup/OA01_LSOA01_MSOA01_EW_LU.csv")
-      
+
 # Remove the numbers from the end of MSOA01NM and we have place names. We can use those 
 # 
 # lookup %>% 
@@ -46,29 +46,42 @@ lookup <- read_csv("data/2001_geographical_lookup/OA01_LSOA01_MSOA01_EW_LU.csv")
 #     place_name = str_trim(place_name)
 #          ) %>% 
 #  group_by(place_name) %>% tally %>% arrange(desc(n)) %>% View
-  
 
-   
-lookup %>% 
-  select(lsoa = LSOA01CD, msoa = MSOA01CD) %>% 
-  distinct() -> simple_lookup
-
-
-dta %>% 
-  left_join(simple_lookup) %>% 
-  select(msoa, year, geography_short, count) %>% 
-  group_by(msoa, year, geography_short) %>% 
-  summarise(count = sum(count)) %>% 
-  ungroup() %>% 
-  group_by(msoa, year) %>% 
-  mutate(proportion = count / sum(count)) -> dta_msoa
 
 # TTWA lookup 
 
-read_csv("data/")
-# 
+ttwa_lookup <- read_csv("data/LSOA01_TTWA01_UK_LU.csv")
+
+ttwa_lookup %>% 
+  select(LSOA01CD, TTWA01CD, TTWA01NM) %>% 
+  inner_join(lookup) %>% 
+  select(lsoa = LSOA01CD, msoa = MSOA01CD, ttwa = TTWA01CD, ttwa_name = TTWA01NM) -> ttwa_msoa_lookup
+
+dta %>% 
+  left_join(ttwa_msoa_lookup) %>% 
+  group_by(ttwa, ttwa_name, msoa, year, geography_short) %>% 
+  summarise(count = sum(count)) %>% 
+  ungroup() %>% 
+  group_by(msoa, year) %>% 
+  mutate(proportion = count / sum(count)) -> dta_msoa_ttwa
+
+dta %>% 
+  left_join(ttwa_msoa_lookup) %>% 
+  group_by(ttwa, ttwa_name, lsoa, year, geography_short) %>% 
+  summarise(count = sum(count)) %>% 
+  ungroup() %>% 
+  group_by(lsoa, year) %>% 
+  mutate(proportion = count / sum(count)) -> dta_lsoa_ttwa
+
+
+# shapefile, MSOA
 
 shp_msoa <- read_shape(file = "shapefiles/Middle_layer_super_output_areas_(E+W)_2001_Boundaries_(Full_Extent)_V2/MSOA_2001_EW_BFE_V2.shp")
+
+
+# Shapefile, LSOA 
+
+shp_lsoa <- read_shape(file = "shapefiles/England_low_soa_2001/england_low_soa_2001.shp")
 
 # # Create cartogram
 # dta_msoa  %>% 
@@ -87,6 +100,59 @@ shp_msoa <- read_shape(file = "shapefiles/Middle_layer_super_output_areas_(E+W)_
 
 # Append proportion UK born in different places 
 
+# Do just for Sheffield, MSOA
+
+dta_msoa_ttwa %>% 
+  filter(year == 2011) %>% 
+  filter(geography_short == "UK") %>% 
+  filter(ttwa_name == "Sheffield and Rotherham") %>% 
+  append_data(shp = shp_msoa, data = ., key.shp = "MSOA01CD", key.data = "msoa", ignore.na =T) %>%  
+  .[!is.na(.$ttwa),]  %>% 
+  tm_shape(.) + 
+  tm_fill("proportion")
+
+# Do just for Sheffield, LSOA
+
+dta_lsoa_ttwa %>% 
+  filter(year == 2001) %>%
+  filter(geography_short == "UK") %>% 
+  filter(ttwa_name == "Sheffield and Rotherham") %>% 
+  append_data(shp = shp_lsoa, data = ., key.shp = "zonecode", key.data = "lsoa", ignore.na = T) %>% 
+  .[!is.na(.$ttwa),] %>% 
+  tm_shape(.) + 
+  tm_fill("proportion")
+
+dta_lsoa_ttwa %>% 
+  filter(year == 2011) %>%
+  filter(geography_short == "UK") %>% 
+  filter(ttwa_name == "Sheffield and Rotherham") %>% 
+  append_data(shp = shp_lsoa, data = ., key.shp = "zonecode", key.data = "lsoa", ignore.na = T) %>% 
+  .[!is.na(.$ttwa),] %>% 
+  tm_shape(.) + 
+  tm_fill("proportion", breaks = seq(0, 1, by = 0.1))
+
+dta_msoa_ttwa %>% 
+  filter(year == 2001) %>% 
+  filter(geography_short == "UK") %>% 
+  filter(ttwa_name == "York") %>% 
+  append_data(shp = shp_msoa, data = ., key.shp = "MSOA01CD", key.data = "msoa", ignore.na =T) %>%  
+  .[!is.na(.$ttwa),]  %>% 
+  tm_shape(.) + 
+  tm_fill("proportion")
+
+
+# For individual cities LSOAs look OK 
+
+dta_msoa_ttwa %>% 
+  filter(year == 2011) %>% 
+  filter(geography_short == "UK") %>% 
+  filter(ttwa_name == "Sheffield and Rotherham") %>% 
+  append_data(shp = shp_msoa, data = ., key.shp = "MSOA01CD", key.data = "msoa", ignore.na =T) %>%  
+  .[!is.na(.$ttwa),]  %>% 
+  tm_shape(.) + 
+  tm_fill("proportion")
+
+
 
 # Plot MSOAs by proportion UK born in 2001
 dta_msoa %>% 
@@ -102,7 +168,7 @@ dta_msoa %>%
     palette = brewer.pal(10, "Spectral"),
     breaks = seq(0, 1.0, by = 0.1), showNA = F,
     border.col = NULL
-    )
+  )
 
 
 # % point change in proportion from 2001 to 2011
@@ -137,9 +203,103 @@ dta_msoa %>%
     palette = brewer.pal(10, "Spectral"),
     breaks = c(0.4, 0.6, 0.9, 0.95, 1.0, 1.05, 1.10, 1.15, 1.20, 1.40, 1.8), showNA = F,
     border.col = NULL
-  ) -> change_uk
+  ) 
 
-tmap_leaflet(change_uk)
+# Tried to start up leaflet. Did not work.... 
+
+# The outlier - 
+# This could be Lakenheath, near RAF Lakenheath
+# RAF Lakenheath is a USAF base. 
+# There could have been a large reduction in the size of US presence 
+# between 2001 and 2011, leading to an increase in the UK born population here. 
+
+#To test this, let's look at the Other proportion
+
+# % point change in proportion from 2001 to 2011
+dta_msoa %>% 
+  filter(geography_short == "Other") %>% 
+  ungroup() %>% 
+  select(msoa, year, proportion) %>% 
+  spread(year, proportion) %>% 
+  rename(p_2001 = `2001`, p_2011 = `2011`) %>% 
+  mutate(change = p_2011 - p_2001) %>% 
+  append_data(shp = shp_msoa, data = ., key.shp = "MSOA01CD", key.data = "msoa", ignore.na =T) %>% 
+  tm_shape(.) + 
+  tm_polygons(
+    "change", 
+    palette = brewer.pal(10, "Spectral"),
+    breaks = seq(-0.25, 0.25, by = 0.05), showNA = F,
+    border.col = NULL
+  )
+# This clearly indicates the same region as having the largest fall in 'Other' population
 
 
+
+# Now, back to looking at change in EU proportion 
+
+dta_msoa %>% 
+  filter(geography_short == "Europe - Not Old EU") %>% 
+  ungroup() %>% 
+  select(msoa, year, proportion) %>% 
+  spread(year, proportion) %>% 
+  rename(p_2001 = `2001`, p_2011 = `2011`) %>% 
+  mutate(change = p_2011 - p_2001) %>% 
+  append_data(shp = shp_msoa, data = ., key.shp = "MSOA01CD", key.data = "msoa", ignore.na =T) %>% 
+  tm_shape(.) + 
+  tm_polygons(
+    "change", 
+    palette = brewer.pal(10, "Spectral"),
+    breaks = seq(-0.25, 0.25, by = 0.05), showNA = F,
+    border.col = NULL
+  )    
+
+# % rather than % point change
+dta_msoa %>% 
+  filter(geography_short == "Europe - Not Old EU") %>% 
+  ungroup() %>% 
+  select(msoa, year, proportion) %>% 
+  spread(year, proportion) %>% 
+  rename(p_2001 = `2001`, p_2011 = `2011`) %>% 
+  mutate(change = (p_2011 + 0.001)  / (p_2001 + 0.001)) %>% # Continuity correction
+  filter(!is.na(change)) %>% 
+  append_data(shp = shp_msoa, data = ., key.shp = "MSOA01CD", key.data = "msoa", ignore.na =T) %>% 
+  tm_shape(.) + 
+  tm_polygons(
+    "change", 
+    palette = brewer.pal(10, "Spectral"),
+    breaks = c(0.5, 1, 2, 5, 10, 20, 50, 100, 200), showNA = F,
+    border.col = NULL
+  )    
+
+dta_msoa %>% 
+  filter(geography_short == "Europe - Old EU or Western Europe") %>% 
+  ungroup() %>% 
+  select(msoa, year, proportion) %>% 
+  spread(year, proportion) %>% 
+  rename(p_2001 = `2001`, p_2011 = `2011`) %>% 
+  mutate(change = (p_2011 + 0.001)  / (p_2001 + 0.001)) %>% # Continuity correction
+  filter(!is.na(change)) %>% 
+  append_data(shp = shp_msoa, data = ., key.shp = "MSOA01CD", key.data = "msoa", ignore.na =T) %>% 
+  tm_shape(.) + 
+  tm_polygons(
+    "change", 
+    palette = brewer.pal(10, "Spectral"),
+    breaks = c(0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 1, 2, 5, 10), showNA = F,
+    border.col = NULL
+  )    
+
+
+# This definitely points to the two population groups being 
+# very different in their migration trends between 2001 and 2011
+
+
+# Sheffield TTWA 
+
+lookup %>% 
+  inner_join(ttwa_lookup)
+  
+  
+  
+  dta_msoa
+dta
 # 
